71adfe593273eb8a6f4225a6ba482e5f5a80f502
Zsombor Szabó <codingoat@gmail.com>
2023-09-17 19:59:03 +0300

    Small fixes
    - Made block more OP by adjusting timing and angle
    - Fixed dropped MoneyPickup being collected instantly

--- Assets/Scripts/Params.cs
+++ Assets/Scripts/Params.cs
@@ -3,7 +3,7 @@ namespace Moneybag
     public static class Params
     {
         public const int bagValue = 3;
-        public const float blockAngle = 45;
+        public const float blockAngle = 120;
         public const int bagsToWin = 5;
         public const float spawnedMoneyMultiplier = .7f;
         public const float gameLength = 100;
